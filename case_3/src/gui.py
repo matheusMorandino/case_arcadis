11a73ae0cb9a89c9ae9ef31e78e7b18425042ea8
matheusMorandino <matheus2060@gmail.com>
2024-01-26 21:47:56 -0300

    Descoplando a lógica do processamento da gui

--- case_3/src/gui.py
+++ case_3/src/gui.py
@@ -1,84 +1,56 @@
 import PySimpleGUI as sg
-from consolidadorTabela import ConsolidadorTabela
-import pandas as pd
-from datetime import datetime
-import sys
-import os
 
 
-def processar_arquivos(input_files: list[str], output_path: str, progress_bar: sg.Window):
-    """
-    Função responsável por chamar o consolidador de arquivos e processar as tabelas
+class GUI:
+    def __init__(self, processar_arquivos=lambda input_files, output_path, progress_bar: None):
+        self.arquivos_selecionados = []
+        self.output_path = ""
 
-    :param input_files:
-    :param output_path:
-    :param progress_bar:
-    :return:
-    """
-    consolidador = ConsolidadorTabela()
-    df_processados = pd.DataFrame(columns=['#sys_loc_code', 'param_code', 'param_value', 'param_unit',
-       'measurement_method', 'measurement_date', 'remark', 'task_code'])
+        # Definindo GUI layout
+        self.layout = [
+            [sg.Text("Selecione o(s) arquivo(s) .xlsx:"), sg.Input(key="-FILES-", enable_events=True),
+             sg.FilesBrowse(file_types=(("Excel Files", "*.xlsx"),))],
+            [sg.Text("Output Path:"), sg.Input(key="-OUTPUT-", enable_events=True), sg.FolderBrowse()],
+            [sg.Button("Processar", key="-RUN-")]
+        ]
 
-    for i, file in enumerate(input_files, start=1):
-        sg.one_line_progress_meter('Processando arquivos:', i, len(input_files), 'key',
-                                   'Arquivo atual: ' + os.path.basename(file))
+        # Criando janela
+        self.window = sg.Window("Case 3", self.layout)
 
-        try:
-            df_auxiliar = consolidador.consolida_arquivo(excel_path=file)
-            df_processados = pd.concat([df_processados, df_auxiliar], ignore_index=True)
-        except Exception as e:
-            sg.popup_error(f"Erro {os.path.basename(file)}: {str(e)}")
-            sys.exit()
+        # Implementando a função de processamento de arquivos
+        self.process_files_callback = processar_arquivos
 
-        progress_bar.update_bar(i, len(input_files))
+    def limpar_campos_input(self):
+        self.window["-FILES-"].update("")
+        self.window["-OUTPUT-"].update("")
 
-    sg.one_line_progress_meter_cancel('key')
+    def get_eventos(self):
+        while True:
+            event, valor = self.window.read()
 
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    output_name = "case_3_consolidado_"+timestamp
+            if event == sg.WIN_CLOSED:
+                break
+            elif event == "-FILES-":
+                self.arquivos_selecionados = valor["-FILES-"].split(";")
+            elif event == "-OUTPUT-":
+                self.output_path = valor["-OUTPUT-"]
+            elif event == "-RUN-":
+                if not self.arquivos_selecionados or not self.output_path:
+                    sg.popup_error("Por favor selecione um caminho para o output e pelo menos um arquivo de input.")
+                else:
+                    progress_layout = [
+                        [sg.Text("Processando arquivos")],
+                        [sg.ProgressBar(len(self.arquivos_selecionados), orientation='h', size=(20, 20), key='-PROGRESS-')],
+                    ]
 
-    # Salvando resultado consolidado como .xslx
-    df_processados.to_excel(os.path.join(output_path, output_name+".xlsx"), index=False)
+                    progress_window = sg.Window("Processando", progress_layout, finalize=True)
+                    progress_bar = progress_window['-PROGRESS-']
 
-# Define GUI layout
-layout = [
-    [sg.Text("Selecione o(s) arquivo(s) .xlsx:"), sg.Input(key="-FILES-", enable_events=True), sg.FilesBrowse(file_types=(("Excel Files", "*.xlsx"),))],
-    [sg.Text("Output Path:"), sg.Input(key="-OUTPUT-", enable_events=True), sg.FolderBrowse()],
-    [sg.Button("Processar", key="-RUN-")]
-]
+                    self.process_files_callback(self.arquivos_selecionados, self.output_path, progress_bar)
+                    self.limpar_campos_input()
 
-# Create the window
-window = sg.Window("Case 3", layout)
+                    progress_window.close()
+                    sg.popup("Processamento completo!")
 
-arquivos_selecionados = []
-output_path = ""
-csv_check = False
-json_check = False
-
-while True:
-    event, values = window.read()
-
-    if event == sg.WIN_CLOSED:
-        break
-    elif event == "-FILES-":
-        arquivos_selecionados = values["-FILES-"].split(";")
-    elif event == "-OUTPUT-":
-        output_path = values["-OUTPUT-"]
-    elif event == "-RUN-":
-        if not arquivos_selecionados or not output_path:
-            sg.popup_error("Por favor selecione um caminho para o output e pelo menos um arquivo de input.")
-        else:
-            progress_layout = [
-                [sg.Text("Processando arquivos")],
-                [sg.ProgressBar(len(arquivos_selecionados), orientation='h', size=(20, 20), key='-PROGRESS-')],
-            ]
-
-            progress_window = sg.Window("Processando", progress_layout, finalize=True)
-            progress_bar = progress_window['-PROGRESS-']
-
-            processar_arquivos(arquivos_selecionados, output_path, progress_bar)
-
-            progress_window.close()
-            sg.popup("Processamento completo!")
-
-window.close()
+    def fechar_janela(self):
+        self.window.close()
---
+++ case_3/src/main.py
@@ -0,0 +1,48 @@
+from gui import GUI
+import PySimpleGUI as sg
+from consolidadorTabela import ConsolidadorTabela
+import pandas as pd
+from datetime import datetime
+import sys
+import os
+
+
+def processar_arquivos(input_files: list[str], output_path: str, progress_bar: sg.Window):
+    """
+    Função responsável por chamar o consolidador de arquivos e processar as tabelas
+
+    :param input_files:
+    :param output_path:
+    :param progress_bar:
+    :return:
+    """
+    consolidador = ConsolidadorTabela()
+    df_processados = pd.DataFrame(columns=['#sys_loc_code', 'param_code', 'param_value', 'param_unit',
+       'measurement_method', 'measurement_date', 'remark', 'task_code'])
+
+    for i, file in enumerate(input_files, start=1):
+        sg.one_line_progress_meter('Processando arquivos:', i, len(input_files), 'key',
+                                   'Arquivo atual: ' + os.path.basename(file))
+
+        try:
+            df_auxiliar = consolidador.consolida_arquivo(excel_path=file)
+            df_processados = pd.concat([df_processados, df_auxiliar], ignore_index=True)
+        except Exception as e:
+            sg.popup_error(f"Erro {os.path.basename(file)}: {str(e)}")
+            sys.exit()
+
+        progress_bar.update_bar(i, len(input_files))
+
+    sg.one_line_progress_meter_cancel('key')
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    output_name = "case_3_consolidado_"+timestamp
+
+    # Salvando resultado consolidado como .xslx
+    df_processados.to_excel(os.path.join(output_path, output_name+".xlsx"), index=False)
+
+
+if __name__ == "__main__":
+    gui = GUI(processar_arquivos=processar_arquivos)
+    gui.get_eventos()
+    gui.fechar_janela()
